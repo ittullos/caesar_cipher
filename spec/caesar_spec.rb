f4656204abfc81b49012a8a1868391c58ea25445
Isaac Tullos <isaac.tullos@gmail.com>
2021-11-04 02:22:34 -0400

    Added some tests to spec file

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -1,23 +1,36 @@
 require './caesar_cipher'
 
-describe 'caesar cipher' do
+describe 'solve' do
   before { @cipher = CaesarCipher.new }
+  it 'shifts a letter by a given value' do
+    expect(@cipher.solve("a", 3)).to eql("d")
+  end
 
-  context 'valid input' do
-    it 'works with What a string! and 5' do
-      expect(@cipher.solve('What a string!', 5)).to eq 'Bmfy f xywnsl!'
-    end
+  it 'shifts a string by a given value' do
+    expect(@cipher.solve("aaa", 3)).to eql("ddd")
+  end
 
-    it 'works with Hello World! and 7' do
-      expect(@cipher.solve('Hello World!', 7)).to eq 'Olssv Dvysk!'
-    end
+  it 'shifts a string by a negative value' do
+    expect(@cipher.solve("ccc", -2)).to eql("aaa")
+  end
 
-    it 'works with Hey, my name is Isaac. and 1' do
-      expect(@cipher.solve('Hey, my name is Isaac.', 1)).to eq 'Ifz, nz obnf jt Jtbbd.'
-    end
+  it 'leaves uppercase and lowercase intact' do
+    expect(@cipher.solve("aBcD", 3)).to eql("dEfG")
+  end
 
-    it 'works with Hi/Hello/A-154 and -2' do
-      expect(@cipher.solve('Hi/Hello/A-154', -2)).to eq 'Fg/Fcjjm/Y-932'
-    end
+  it 'leaves punctuation and symbols intact' do
+    expect(@cipher.solve("abc!@><", 3)).to eql("def!@><")
+  end
+
+  it 'accounts for spaces in the string' do
+    expect(@cipher.solve("What a string!", 3)).to eql("Zkdw d vwulqj!")
+  end
+
+  it 'wraps from the end of the alphabet to the beginning' do
+    expect(@cipher.solve("zyz", 2)).to eql("bab")
+  end
+
+  it 'wraps from the beginning of the alphabet to the end' do
+    expect(@cipher.solve("aba", -2)).to eql("yzy")
   end
 end
