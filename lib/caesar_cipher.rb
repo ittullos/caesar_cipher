dc5a287ee8801c1493629e4e90d3d5813f6eeda5
Isaac Tullos <isaac.tullos@gmail.com>
2021-11-09 12:33:17 -0500

    Changed logic in key method
    The key method now directly accesses the character set arrays as a
    multi dimensional array instead of passing all three constants to
    key using the splat operator. This makes several lines of code
    unnecessary and makes the code much shorter. The key method also uses
    reduce instead of each for iterating through the character sets.

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -5,19 +5,16 @@ NUMERICAL = ('0'..'9').to_a
 class CaesarCipher
 
   def solve(message, shift)
-    key = key(shift,LOWER_CASE,UPPER_CASE,NUMERICAL)
-    solution(key, message)
+    solution(key(shift), message)
   end
 
   private
 
-  def key(shift, *character_sets)
-    key = {}
-    character_sets.each do |set|
+  def key(shift)
+    [LOWER_CASE,UPPER_CASE,NUMERICAL].reduce({}) do |key, set|
       key_pairs = set.zip(set.rotate(shift)).to_h
-      key = key.merge(key_pairs)
+      key.merge(key_pairs)
     end
-  key
   end
 
   def solution(key, message)
